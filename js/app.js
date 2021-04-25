c5b1b9c8cbd2949dbe402c5f424b31cd7ce701fd
Biaku- <adriandlc81998@gmail.com>
2021-04-25 12:27:44 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+    document.querySelector(".container-portada").addEventListener("click",changeImg);
+
+function changeImg(){
+    const arrayImg= [
+        "http://www.wedoitmarketing.com/wp-content/uploads/2015/10/1-3.jpg",
+        "https://covertalavera.com/wp-content/uploads/2021/02/office-381228_1920.jpg",
+        "https://www.iberopuebla.mx/sites/default/files/slidp/img-bg/00_img_d._inteligencia_empresarial_copy.jpg"
+    ];
+    const portada = document.querySelector(".container-portada");
+    portada.style.backgroundImage=`url(${arrayImg[0]})`;
+};
