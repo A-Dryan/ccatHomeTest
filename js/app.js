a24ad24809a3025fbfcadc6c0de613d760b524bf
Biaku- <adriandlc81998@gmail.com>
2021-05-01 14:36:43 -0500

    commit2

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@ const app = new Vue({
     el: '#app',
 data:{
     slidesof:[    
-    {imgurl: 'http://www.wedoitmarketing.com/wp-content/uploads/2015/10/1-3.jpg'},
-    {imgurl: 'https://i.pinimg.com/originals/07/08/17/070817e1cc30dcb7b6c37951ca43cbf1.jpg'},
-    {imgurl: 'https://covertalavera.com/wp-content/uploads/2021/02/office-381228_1920.jpg'},
-    {imgurl: 'https://www.iberopuebla.mx/sites/default/files/slidp/img-bg/00_img_d._inteligencia_empresarial_copy.jpg'}
+    {imgurl: 'https://wallpapercave.com/wp/wp7728133.jpg'},
+    {imgurl: 'https://fondosmil.com/fondo/69976.jpg'},
+    {imgurl: 'https://wallpx.com/image/2021/04/macbook-air-business-desktop-computer.jpg'},
+    {imgurl: 'https://miro.medium.com/max/3200/1*8xSZjQ8Zk--_wx9anAGLXQ.jpeg'},
+    {imgurl: 'https://lh5.googleusercontent.com/FIUN1ETn9DDZ73P3laU7Gx_Veoox0Z1idcDDo8rKk-oMs1fUP5Oubk7ogjmJ06KgpSRTic4wXE1c2nvXjhS8eu2uQYwBkWP_RBFE7r_-UB1KGMLmHDWrwJehpS8Q_c8kIE1_ykE'},
+    {imgurl: 'https://www.teahub.io/photos/full/224-2249735_most-important-metric-tracked-for-database-performance-2019.jpg'}
     ]
 }
 });
